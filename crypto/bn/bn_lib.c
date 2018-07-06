90836929f3a71fb30ef415b9e3028b3d111de95c
Andy Polyakov <appro@openssl.org>
2018-07-06 13:16:40 +0200

    bn/bn_lib.c: remove bn_check_top from bn_expand2.
    Trouble is that addition is postponing expansion till carry is
    calculated, and if addition carries, top word can be zero, which
    triggers assertion in bn_check_top.
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/6810)
    
    (cherry picked from commit e42395e637c3507b80b25c7ed63236898822d2f1)
    
    Resolved conflicts:
    	crypto/bn/bn_lib.c

--- crypto/bn/bn_lib.c
+++ crypto/bn/bn_lib.c
@@ -289,8 +289,6 @@ static BN_ULONG *bn_expand_internal(const BIGNUM *b, int words)
     const BN_ULONG *B;
     int i;
 
-    bn_check_top(b);
-
     if (words > (INT_MAX / (4 * BN_BITS2))) {
         BNerr(BN_F_BN_EXPAND_INTERNAL, BN_R_BIGNUM_TOO_LONG);
         return NULL;
@@ -424,8 +422,6 @@ BIGNUM *bn_dup_expand(const BIGNUM *b, int words)
 
 BIGNUM *bn_expand2(BIGNUM *b, int words)
 {
-    bn_check_top(b);
-
     if (words > b->dmax) {
         BN_ULONG *a = bn_expand_internal(b, words);
         if (!a)
@@ -459,7 +455,6 @@ BIGNUM *bn_expand2(BIGNUM *b, int words)
         assert(A == &(b->d[b->dmax]));
     }
 #endif
-    bn_check_top(b);
     return b;
 }
 
