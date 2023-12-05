56445002a1c29745d36e4690173550ec4824613c
Neil Horman <nhorman@openssl.org>
2023-12-05 15:24:20 -0500

    Harden asn1 oid loader to invalid inputs
    In the event that a config file contains this sequence:
    =======
    openssl_conf = openssl_init
    
    config_diagnostics = 1
    
    [openssl_init]
    oid_section = oids
    
    [oids]
    testoid1 = 1.2.3.4.1
    testoid2 = A Very Long OID Name, 1.2.3.4.2
    testoid3 = ,1.2.3.4.3
    ======
    
    The leading comma in testoid3 can cause a heap buffer overflow, as the
    parsing code will move the string pointer back 1 character, thereby
    pointing to an invalid memory space
    
    correct the parser to detect this condition and handle it by treating it
    as if the comma doesn't exist (i.e. an empty long oid name)
    
    (cherry picked from commit a552c23c6502592c1b3c67d93dd7e5ffbe958aa4)
    
    Reviewed-by: Tom Cosgrove <tom.cosgrove@arm.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/23034)
    
    (cherry picked from commit d802bfbf80bab00123a4a6209f255852b3a10207)

--- crypto/asn1/asn_moid.c
+++ crypto/asn1/asn_moid.c
@@ -67,6 +67,11 @@ static int do_create(const char *value, const char *name)
     if (!p) {
         ln = name;
         ostr = value;
+    } else if (p == value) {
+        /* we started with a leading comma */
+        ln = name;
+        ostr = p + 1;
+        p = NULL;
     } else {
         ln = NULL;
         ostr = p + 1;
