f020eb0156e232689ebbae38b9c121d5007581e5
Timothy Copeland <tacopeland@proton.me>
2025-12-02 16:05:30 +1100

    check_cert_crl(): Set CRL score for CRLs returned by get_crl callback
    Reviewed-by: Nikola Pajkovsky <nikolap@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/29199)
    
    (cherry picked from commit c7a657d8007853791f27235a176131ad1daf358a)

--- crypto/x509/x509_vfy.c
+++ crypto/x509/x509_vfy.c
@@ -997,9 +997,14 @@ static int check_cert(X509_STORE_CTX *ctx)
     while (ctx->current_reasons != CRLDP_ALL_REASONS) {
         last_reasons = ctx->current_reasons;
         /* Try to retrieve relevant CRL */
-        if (ctx->get_crl)
+        if (ctx->get_crl) {
             ok = ctx->get_crl(ctx, &crl, x);
-        else
+            if (crl)
+                ctx->current_crl_score = get_crl_score(ctx,
+                                                       &ctx->current_issuer,
+                                                       &ctx->current_reasons,
+                                                       crl, x);
+	} else
             ok = get_crl_delta(ctx, &crl, &dcrl, x);
         /*
          * If error looking up CRL, nothing we can do except notify callback
